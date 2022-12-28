45d2d587fa05823cd500446ff5618bcc0a3e1acb
Un1tee <93337732+Un1tee@users.noreply.github.com>
2022-12-28 22:33:03 +0200

    Add files via upload

---
+++ Lesson1.java
@@ -0,0 +1,18 @@
+
+public class Lesson1 {
+    public static void main(String[] args){
+
+        String name;        //task 1
+        int age;
+        String city;
+
+        int today_Year = 2020;         //task 2
+        int friends_Age = 18;
+
+        int friends_bYear = today_Year - friends_Age;
+
+        System.out.println(friends_bYear);
+
+
+    }
+}
